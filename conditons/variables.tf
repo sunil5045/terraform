49b3ab50addb9b83c90e99379531cd9f0cca13da
sunilreddy <sunilsmec@gmail.com>
2026-03-25 21:09:39 +0530

    terraform

---
+++ conditons/variables.tf
@@ -0,0 +1,60 @@
+variable "environment" {
+  default = "dev"
+}
+
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+    description = "RHEL 9 Image"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+  description = "instance type"
+}
+
+variable "ec2_tags" {
+  type = map
+  default = {
+    Name = "variables-demo"
+    Project = "roboshop"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+variable "sg_name" {
+  type = string
+  default = "allow-all-terraform"
+}
+
+variable "sg_descriptoin" {
+  type = string
+  default = "Allow TLS inbound traffic and all outbound traffic"  
+}
+
+variable "sg_from_port" {
+  type = number
+  default = 0
+}
+
+variable "sg_to_port" {
+  type = number
+  default = 0
+}
+
+variable "cidr_blocks" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+  type = map
+  default = {
+    Name = "allow-all-terraform"
+    Project = "Roboshop"
+    terraform = "true"
+    Environment = "dev"
+  }
+  }
